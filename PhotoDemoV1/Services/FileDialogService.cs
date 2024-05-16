96a77985718e28414a52dc8f76564a51bc0082a9
HRX <315157286@qq.com>
2024-05-16 23:23:53 +0800

    新增选择文件夹功能

---
+++ PhotoDemoV1/Services/FileDialogService.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoDemoV1.Services;
+
+public class FileService
+{
+    private static readonly Lazy<FileService> lazy = new(() => new FileService());
+    public static FileService Instance => lazy.Value;
+
+    /// <summary>
+    /// 以默认选项打开文件夹
+    /// </summary>
+    /// <param name="view">基点视图</param>
+    /// <returns>文件夹路径集合</returns>
+    public async Task<IReadOnlyList<IStorageFolder>> OpenFolderAsync(TopLevel view)
+    {
+        FolderPickerOpenOptions options = new()
+        {
+            Title = "选择文件夹"
+        };
+        return await OpenFolderAsync(view, options);
+    }
+
+    /// <summary>
+    /// 以自定义选项打开文件夹
+    /// </summary>
+    /// <param name="view">基点视图</param>
+    /// <param name="options">自定义选项</param>
+    /// <returns>文件夹路径集合</returns>
+    public async Task<IReadOnlyList<IStorageFolder>> OpenFolderAsync(TopLevel view, FolderPickerOpenOptions options)
+    {
+        return await view.StorageProvider.OpenFolderPickerAsync(options);
+    }
+}
--- PhotoDemoV1/ViewModels/Pages/HomeViewModel.cs
+++ PhotoDemoV1/ViewModels/Pages/HomeViewModel.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using Avalonia.VisualTree;
+using PhotoDemoV1.Services;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +14,54 @@ namespace PhotoDemoV1.ViewModels;
 
 internal class HomeViewModel : ViewModelBase
 {
-    
+    private List<string> inputPaths = [];
+    public List<string> InputPaths
+    {
+        get => inputPaths;
+        set => this.RaiseAndSetIfChanged(ref inputPaths, value);
+    }
+
+    private string outputPath = "";
+    public string OutputPath
+    {
+        get => outputPath;
+        set => this.RaiseAndSetIfChanged(ref outputPath, value);
+    }
+
+    public async void OpenInputFolderCommand(Control view)
+    {
+        //通过传递过来的控件获取其顶层视图(一般为 MainWindow)
+        TopLevel? topLevel = (TopLevel?)view.GetVisualRoot();
+        if(topLevel is null)
+        {
+            return;
+        }
+        FolderPickerOpenOptions options = new()
+        {
+            Title = "选择输入文件夹(多选)",
+            AllowMultiple = true,
+        };
+        //通过顶层视图异步打开窗口
+        var paths = await FileService.Instance.OpenFolderAsync(topLevel, options);
+        InputPaths = paths.Select(x => x.Path.ToString()).ToList();
+    }
+
+    public async void OpenOutputFolderCommand(Control view)
+    {
+        //通过传递过来的控件获取其顶层视图(一般为 MainWindow)
+        TopLevel? topLevel = (TopLevel?)view.GetVisualRoot();
+        if (topLevel is null)
+        {
+            return;
+        }
+        FolderPickerOpenOptions options = new()
+        {
+            Title = "选择输出文件夹",
+        };
+        //通过顶层视图异步打开窗口
+        var paths = await FileService.Instance.OpenFolderAsync(topLevel, options);
+        if (paths.Any())
+            OutputPath = paths.First().Path.ToString();
+        OutputPath = Environment.CurrentDirectory + "\\tempOutput";
+    }
 }
